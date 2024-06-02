585f35e69feffff8c4a96eb91bc921091bc07417
vokirok <pavel.korikov@gmail.com>
2024-06-02 10:46:29 +0700

    working with cart

--- src/pages/Debug.vue
+++ src/pages/Debug.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref } from 'vue';
-import { useFirestore, useCollection, useDocument } from 'vuefire';
-import { collection, doc, setDoc } from 'firebase/firestore';
+import { ref, computed, onMounted, onBeforeUnmount, watchEffect } from 'vue';
+import { useCurrentUser, useFirestore, useCollection, useDocument } from 'vuefire';
+import { collection, doc, getDoc, setDoc, onSnapshot, where, query } from 'firebase/firestore';
 
 const firestore = useFirestore();
 
+const user = useCurrentUser();
 const products = useCollection(collection(firestore, 'products'));
 
 const numTestProducts = 52;
@@ -73,7 +74,82 @@ async function uploadJsonToFirebase() {
     .then(jsonToFirebase);
 }
 
-const productToDisplay = ref({ id: 52, name: 'The product' });
+// const productToDisplay = ref({ id: 52, name: 'The product' });
+
+const cart = ref(new Set());
+
+const cartItems = computed(() => {
+  return Array.from(cart.value);
+});
+
+function updateCart() {
+  if (user.value) {
+    collection(firestore, 'products');
+
+    setDoc(doc(firestore, 'cart', String(user.value.uid)), { cart: Array.from(cart.value) });
+  }
+}
+
+function addToCart(product) {
+  cart.value.add(product);
+  updateCart();
+}
+
+function removeFromCart(product) {
+  cart.value.delete(product);
+  updateCart();
+}
+
+// const cartInFirebase = useCollection(collection(firestore, 'cart', user.value.uid));
+
+function checkout() {
+  if (user.value) {
+    console.log(user.value.uid);
+  }
+}
+
+// let unsubscribe = null;
+
+watchEffect(() => {
+  if (user.value) {
+    console.log(`watchEffect: ${user.value.uid}`);
+
+    getDoc(doc(firestore, 'cart', String(user.value.uid))).then((doc) => {
+      if (doc) {
+        const data = doc.data();
+        if (data) {
+          const c = data.cart;
+          if (c) {
+            cart.value = new Set(doc.data().cart);
+          }
+        }
+        console.log('Doc exists:');
+        console.log(doc);
+        console.log(data);
+        // cart.value = new Set(doc.data().cart);
+      } else {
+        console.log('Doc is not exists');
+      }
+    });
+
+    // const q = query(collection(firestore, 'cart'), where('uid', '==', user.value.uid));
+
+    // unsubscribe = onSnapshot(q, (querySnapshot) => {
+    //   const cart = [];
+    //   querySnapshot.forEach((doc) => {
+    //     // cart.push(doc.data().name);
+    //     console.log(cart);
+    //   });
+    //   // console.log('Current cities in CA: ', cart.join(', '));
+    // });
+  }
+});
+
+onBeforeUnmount(() => {
+  // if (unsubscribe) {
+  //   unsubscribe();
+  // }
+});
 </script>
 
 <template>
@@ -101,13 +177,75 @@ const productToDisplay = ref({ id: 52, name: 'The product' });
 
     <Divider />
 
-    <ProductImage :product="productToDisplay" width="250" preview />
+    <p v-if="user">{{ user.displayName }}</p>
 
-    <ul>
-      <li v-for="product in products" :key="product.id">
+    <DataTable :value="cartItems" showGridlines tableStyle="min-width: 30rem">
+      <Column field="id" header="Id"></Column>
+      <Column field="name" header="Name"></Column>
+    </DataTable>
+
+    <Divider />
+
+    <!-- <ProductImage :product="productToDisplay" width="250" preview /> -->
+
+    <DataTable :value="products" showGridlines tableStyle="min-width: 30rem">
+      <Column field="id" header="Id"></Column>
+      <Column header="Image">
+        <template #body="slotProps">
+          <ProductImage :product="slotProps.data" imageClass="w-6rem border-round" preview />
+        </template>
+      </Column>
+      <Column field="name" header="Name">
+        <template #body="slotProps">
+          <div>
+            <span class="font-medium text-secondary text-sm">{{ slotProps.data.category }}</span>
+            <router-link :to="`/product/${slotProps.data.id}`" class="no-underline">
+              <div class="text-lg font-medium text-900 mt-2">{{ slotProps.data.name }}</div>
+            </router-link>
+            <Rating :modelValue="slotProps.data.rating" readonly :cancel="false" />
+          </div>
+        </template>
+      </Column>
+      <Column field="price" header="Price">
+        <template #body="slotProps">
+          <div class="flex flex-column md:align-items-end gap-5">
+            <span class="text-xl font-semibold text-900">${{ slotProps.data.price }}</span>
+            <div v-if="cart.has(slotProps.data)" class="flex flex-row-reverse md:flex-row gap-2">
+              <Button
+                icon="pi pi-times-circle"
+                v-tooltip="`Remove from cart`"
+                outlined
+                severity="danger"
+                @click="() => removeFromCart(slotProps.data)"
+              ></Button>
+              <Button
+                icon="pi pi-check-square"
+                label="Checkout"
+                :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"
+                class="flex-auto md:flex-initial white-space-nowrap"
+                severity="success"
+                @click="() => checkout()"
+                cla
+              ></Button>
+            </div>
+            <div v-else class="flex flex-row-reverse md:flex-row gap-2">
+              <Button
+                icon="pi pi-cart-arrow-down"
+                label="Add to cart"
+                :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"
+                class="flex-auto md:flex-initial white-space-nowrap"
+                @click="() => addToCart(slotProps.data)"
+              ></Button>
+              <!-- <Button icon="pi pi-heart" outlined></Button> -->
+            </div>
+          </div>
+        </template>
+      </Column>
+    </DataTable>
+
+    <!-- <div v-for="product in products" :key="product.id">
         <router-link :to="`/product/${product.id}`">{{ product.name }}</router-link>
         <span>{{ product.price }}</span>
-      </li>
-    </ul>
+      </li> -->
   </section>
 </template>
